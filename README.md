f5c9f36dc911feb347d014064c73d34115b16e43
JuliaSun623 <cnsdytsy@gmail.com>
2020-07-04 11:04:44 +0800

    add register&login

--- README.md
+++ README.md
@@ -1,1 +1,137 @@
 # Backend
+
+## 登录注册
+
+### 路径
+
+Backend/login
+
+### 完成进度
+
+登录、注册和登出的接口已写好
+
+修改密码和忘记密码的接口已写好
+
+注册邮箱验证和重置（忘记）密码邮箱验证接口已写好
+
+### 调用方法
+
+#### 登录
+
+URL：
+
+http方法：POST
+
+前端需要提供的json数据参数，格式如下：
+
+```json
+{
+    "username": "",		/*用户名*/
+    "password": "",		/*密码*/
+}
+```
+
+当登录成功时，后端返回的json数据格式如下：
+
+```json
+{
+    "status": "ok",						/*ok和error分别对应登录成功与登录失败*/
+    "type": "account",					/*当status为error时，type显示具体错误信息；ok则为account*/
+    "currentAuthority": "user/admin"	/*用户权限分为user和admin*/
+}
+```
+
+#### 注册
+
+URL：
+
+http方法：POST
+
+前端需要提供的json数据参数，格式如下：
+
+```json
+{
+    "username": "",		/*用户名*/
+    "password1": "",	/*密码*/
+    "password2": "",	/*重复密码，需要两次密码一致才可成功注册*/
+    "email": "",		/*邮箱，一个邮箱只能注册一个账号*/
+    "authority": "",	/*用户权限，分为user和admin*/
+}
+```
+
+当注册成功时，后端返回的json数据格式如下：
+
+```json
+{
+    "status": "ok",						/*ok和error分别对应注册成功与注册失败*/
+    "type": "register",					/*当status为error时，type显示具体错误信息；ok则为register*/
+}
+```
+
+后端会生成一个验证码，并向该账号注册的邮箱发送一封包含验证链接的邮件。用户点击链接后即可完成验证。
+
+#### 登出
+
+URL：
+
+http方法：POST
+
+前端调用该接口时，需要处于登录状态，并将登录状态下的cookie传给后端。
+
+登出成功时，后端返回的json数据格式如下：
+
+```json
+{
+    "status": "ok",						/*ok和error分别对应登出成功与登出失败*/
+    "type": "logout",					/*当status为error时，type显示具体错误信息；ok则为logout*/
+}
+```
+
+#### 修改密码
+
+URL：
+
+http方法：POST
+
+前端在调用该接口时，用户应当处于登录状态，并将登录状态下的cookies传给后端。前端需要提供的json数据参数，格式如下：
+
+```json
+{
+    "oldpsw": "",	/*现有密码，用来验证*/
+    "newpsw": "",	/*修改后的密码*/
+}
+```
+
+**注：新旧密码不能相同，新密码不能为空。**
+
+当修改成功时，后端返回的json数据格式如下：
+
+```json
+{
+    "status": "ok",						/*ok和error分别对应修改成功与修改失败*/
+    "type": "changePassword",			/*当status为error时，type显示具体错误信息；ok则为changePassword*/
+}
+```
+
+#### 忘记密码
+
+URL：
+
+http方法：POST
+
+当登录时选择忘记密码，后端会随机生成重置密码发送给用户绑定的邮箱。前端需要提供的json数据参数，格式如下：
+
+```json
+{
+    "username": "",	/*重置密码的账号用户名*/
+}
+```
+
+后端返回的json数据格式如下：
+
+```json
+{
+    "status": "ok",						/*ok和error分别对应重置成功与重置失败*/
+    "type": "changePassword",			/*当status为error时，type显示具体错误信息；ok则为resetPassword*/
+}
+```
---
+++ login/views.py
@@ -0,0 +1,243 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+import json, hashlib, datetime, validators, pytz
+from . import models
+
+
+def myJsonResponse(ret):
+    json_data = json.dumps(ret, ensure_ascii=False)
+    response = HttpResponse(json_data)
+    return response
+
+
+def dictFailLogin(s):
+    """
+    fail login message dictionary
+    """
+    return {'status': 'error',
+            'type': s,
+            'currentAuthority': 'guest'}
+
+
+def dictFail(s):
+    """fail status"""
+    return {'status': 'error',
+            'type': s}
+
+
+def hash_code(s, salt='login_hash'):
+    """
+    encode password
+    """
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())
+    return h.hexdigest()
+
+
+def makeConfrimString(user):
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    code = hash_code(user.name, now)
+    models.ConfirmString.objects.create(code=code, user=user,)
+    return code
+
+
+def sendRegisterEmail(email, username, code):
+    subject = 'Registration Confirm for {}'.format(username)
+    textContent = 'This is a registration confirmation.'
+    htmlContent = '''<p>Click <a href="http://{}/user/confirm/?code={}" target="blank">confirm link</a>
+                    to accomplish the confirmation.</p>'''.format('localhost:8001', code, settings.CONFIRM_DAYS)
+    message = EmailMultiAlternatives(subject, textContent, settings.DEFAULT_FROM_EMAIL, [email])
+    message.attach_alternative(htmlContent, 'text/html')
+    message.send()
+
+
+def sendResetEmail(email, username, newpsw):
+    subject = 'Reset Password for: {}'.format(username)
+    textContent = 'This includes a reset password for user {}'.format(username)
+    htmlContent = '''<p>This includes a reset password for user {}.</p>
+                    <p>Your temporary password is {}. Please change it after login.</p>'''.format(username, newpsw)
+    message = EmailMultiAlternatives(subject, textContent, settings.DEFAULT_FROM_EMAIL, [email])
+    message.attach_alternative(htmlContent, 'text/html')
+    message.send()
+
+
+def userConfirm(request):
+    code = request.GET.get('code', None)
+    message = ''
+    try:
+        confirm = models.ConfirmString.objects.get(code=code)
+    except:
+        message = 'Invalid confirm request!'
+        return render(request, 'login/confirm.html', locals())
+    
+    created_time = confirm.created_time
+    now = datetime.datetime.now()
+    now = now.replace(tzinfo=pytz.timezone('UTC'))
+    old = created_time + datetime.timedelta(settings.CONFRIM_DAYS)
+    if now>old:
+        confirm.user.delete()
+        message = 'Your email expired. Please register again.'
+        return render(request, 'login/confirm.html', locals())
+    else:
+        confirm.user.has_confirmed = True
+        confirm.user.save()
+        confirm.delete()
+        message = 'Successfully confirmed.'
+        return render(request, 'login/confirm.html', locals())
+
+
+@csrf_exempt
+def login(request):
+    """
+    login function
+    """
+    if request.session.get('is_login', None):
+        return myJsonResponse(dictFailLogin('Already login.'))
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['username']
+        password = data['password']
+        try:
+            user = models.User.objects.get(name=username)
+            if user.has_comfirmed == False:
+                message = 'The account named {} has not accomplished email confirmation'.format(username)
+                return myJsonResponse(dictFailLogin(message))
+            if user.password == hash_code(password):
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
+                ret = {'status': 'ok',
+                        'type': 'account',
+                        'currentAuthority': user.authority}
+                return myJsonResponse(ret)
+            else:
+                message = 'Wrong password for user {}'.format(username)
+                return myJsonResponse(dictFailLogin(message))
+        except:
+            message = 'Username not existed.'
+            return myJsonResponse(dictFailLogin(message))
+    else:
+        return myJsonResponse(dictFailLogin('Request method is not POST'))
+
+
+@csrf_exempt
+def register(request):
+    """
+    register function
+    """
+    request.session.clear_expired()
+    if request.session.get('is_login', None):
+        return myJsonResponse(dictFail('Already login.'))
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['username']
+        password1 = data['password1']
+        password2 = data['password2']
+        email = data['email']
+        authority = data['authority']
+        if username == '':
+            message = 'Username cannot be null.'
+            return myJsonResponse(dictFail(message))
+        if password1 != password2:
+            message = 'Two password inputs do not match.'
+            return myJsonResponse(dictFail(message))
+        elif password1 == '':
+            message = 'Password cannot be null.'
+            return myJsonResponse(dictFail(message))
+        if not validators.email(email):
+            message = 'Invalid email address.'
+            return myJsonResponse(dictFail(message))
+        if authority not in {'user', 'admin'}:
+            message = 'Invalid authority.'
+            return myJsonResponse(dictFail(message))
+        same_email_user = models.User.objects.filter(email=email)
+        if same_email_user:
+            message = 'Email address \"{}\" has been used.'.format(email)
+            return myJsonResponse(dictFail(message))
+        else:
+            new_user = models.User.objects.create(
+                name = username,
+                password = hash_code(password1),
+                email = email,
+                has_confirmed = False,
+                authority = authority,
+            )
+            code = makeConfrimString(new_user)
+            sendRegisterEmail(email, username, code)
+            return myJsonResponse({'status': 'ok',
+                                   'type': 'register'})
+    else:
+        return myJsonResponse(dictFailLogin('Request method is not POST'))
+
+
+@csrf_exempt
+def logout(request):
+    if not request.session.get('is_login', None):
+        return myJsonResponse(dictFail('Already logout.'))
+    request.session.flush()
+    return myJsonResponse({'status': 'ok',
+                           'type': 'logout'})
+
+
+@csrf_exempt
+def getCurrentUser(request):
+    if not request.session.get('is_login', None):
+        return myJsonResponse(dictFail('Already logout.'))
+    return myJsonResponse({'status': 'ok',
+                           'username': request.session['user_name']})
+
+
+@csrf_exempt
+def changePassword(request):
+    if not request.session.get('is_login', None):
+        return myJsonResponse(dictFail('Already logout.'))
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['userName']
+        oldpsw, newpsw = data['oldpsw'], data['newpsw']
+        if newpsw == '':
+            return myJsonResponse(dictFail('Invalid new password'))
+        if newpsw == oldpsw:
+            return myJsonResponse(dictFail('New password cannot be the same with old password.'))
+        try:
+            user = models.User.objects.get(name=username)
+        except:
+            return myJsonResponse(dictFail('Username {} not existed.'.format(username)))
+        if user.password == hash_code(oldpsw):
+            user.password = hash_code(newpsw)
+            user.save()
+            request.session.flush()
+            return myJsonResponse({'status': 'ok',
+                                    'type': 'changePassword'})
+        else:
+            return myJsonResponse(dictFail('Wrong password.'))
+    else:
+        return myJsonResponse(dictFail('Request method is not POST.'))
+
+
+@csrf_exempt
+def resetPassword(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['userName']
+        try:
+            user = models.User.objects.get(name=username)
+        except:
+            return myJsonResponse(dictFail('Username {} not existed.'.format(username)))
+        if user.has_comfirmed == False:
+            return myJsonResponse(dictFail('This account {} has not accomplished email confirmation.'.format(username)))
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%M')
+        newpsw = hash_code(username, now)[:16]
+        sendResetEmail(user.email, username, newpsw)
+        user.password = hash_code(newpsw)
+        user.save()
+        return myJsonResponse({'status': 'ok',
+                               'type': 'resetPassword'})
+    else:
+        return myJsonResponse(dictFail('Request method is not POST.'))
+
+        
